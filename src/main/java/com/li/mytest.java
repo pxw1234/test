3222592b6d590e743b1bf50057b265c2a5aaaa28
pxw <982723082@qq.com>
2023-05-07 15:13:54 +0800

    idea次提交

--- src/main/java/com/li/mytest.java
+++ src/main/java/com/li/mytest.java
@@ -3,5 +3,7 @@ package com.li;
 public class mytest {
     public static void main(String[] args) {
         System.out.println("hhhh");
+        System.out.println("第二次提交");
+        System.out.println("idea");
     }
 }
